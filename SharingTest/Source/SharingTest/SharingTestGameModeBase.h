283836df577e993790300f06542ab4547b69172e
DESKTOP-DJEDVCJ\user <kyjooong@gmail.com>
2018-04-26 20:45:33 +0900

    00 Default project

---
+++ SharingTest/Source/SharingTest/SharingTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SharingTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHARINGTEST_API ASharingTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
